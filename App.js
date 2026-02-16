5c5364b67ec6f5bc27d66d03b5a1f75fca499b82
Xexer Ibrahimli <xezerib2008@gmail.com>
2026-02-16 06:03:14 +0400

    Test-3

---
+++ App.js
@@ -0,0 +1,9 @@
+const counter = document.getElementById("counter")
+const plus = document.getElementById("plus")
+const minu = document.getElementById("minu")
+
+plus.addEventListener("click", (e)=>{
+    let count = Number(counter.textContent)
+    count++
+    counter.textContent = count
+})
